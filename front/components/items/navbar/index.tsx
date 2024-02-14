08e448ba9bdcaf08012995017068cf8342b486bc
mavjz28 <maja.miaskiewicz@gmail.com>
2024-02-14 15:25:25 +0100

    fix: rename assets to public directory

--- front/components/items/navbar/index.tsx
+++ front/components/items/navbar/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import ImageFramed from '../image-framed';
 
 const Navbar = () => {
-    return <div className="w-full h-10 bg-black sticky top-0"></div>;
+    return (
+        <div className="w-full h-10 bg-black sticky top-0">
+            <ImageFramed className="w-10 h-7" alt="Black'n'White Tune" src="/logo-picture.png" />
+        </div>
+    );
 };
 
 export default Navbar;
