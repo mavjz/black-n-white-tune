465ff0a3b67751444d35ec08bf9f58ee99bc8e0e
mavjz28 <maja.miaskiewicz@gmail.com>
2024-02-16 10:34:19 +0100

    fix: button type unnecessary when never

--- front/components/items/button/models.ts
+++ front/components/items/button/models.ts
@@ -1,18 +1,25 @@
-export type ButtonType = {
-    isLink?: boolean;
-    isImage?: boolean;
-    className: string;
-} & (
-    | { isLink: true; link: string; onClick: never; buttonType: never; buttonClassName: never }
+export type ButtonType =
     | {
-          isLink: false;
-          link: never;
-          onClick: () => {};
-          buttonType: 'button' | 'submit' | 'reset' | undefined;
-          buttonClassName: string;
-      }
-) &
-    (
-        | { isImage: true; image: string; alt: string; text: never }
-        | { isImage: false; image: never; alt: never; text: string }
-    );
+          isLink?: boolean;
+          isImage?: boolean;
+          className: string;
+      } & (
+          | {
+                isLink: true;
+                link: string;
+                onClick?: never;
+                buttonType?: never;
+                buttonClassName?: never;
+            }
+          | {
+                isLink: false;
+                link?: never;
+                onClick: () => {};
+                buttonType: 'button' | 'submit' | 'reset' | undefined;
+                buttonClassName: string;
+            }
+      ) &
+          (
+              | { isImage: true; image: string; alt: string; text?: never }
+              | { isImage: false; image?: never; alt?: never; text: string }
+          );
--- front/components/items/image-framed/index.tsx
+++ front/components/items/image-framed/index.tsx
@@ -1,11 +1,20 @@
 import Image from 'next/image';
 import React from 'react';
 import { ImageFramedType } from './models';
+import Button from '../button';
 
 const ImageFramed = ({ src, alt, className }: ImageFramedType) => {
     return (
         <div className={`relative ` + className}>
             <Image src={src} alt={alt} fill />
+            <Button
+                isImage
+                image="/Zdjęcie-koszyka"
+                alt="Shop cart"
+                isLink
+                link="/Link-do-sklepu"
+                className=""
+            />
         </div>
     );
 };
