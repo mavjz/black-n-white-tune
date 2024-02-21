ab803a558a4f70d4f8a4ed77d7d3f5e96ffa0d77
mavjz28 <maja.miaskiewicz@gmail.com>
2024-02-21 16:30:23 +0100

    feat: productCard mobile view + animation

--- front/components/items/product-card/index.tsx
+++ front/components/items/product-card/index.tsx
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../button';
+import { motion } from 'framer-motion';
+
+const hoverVariant = {
+    open: {
+        scale: 1.2,
+        transition: {
+            duration: 1,
+        },
+    },
+    closed: {
+        scale: 1,
+        transition: {
+            duration: 1,
+        },
+    },
+};
 
 const ProductCard = () => {
+    const [isHovering, setIsHovered] = useState(false);
+    const onMouseEnter = () => setIsHovered(true);
+    const onMouseLeave = () => setIsHovered(false);
     return (
-        <div className="relative rounded-full">
-            <div className="bg-gradient-to-t from-black absolute bottom-0">
-                <p>Grand pianos</p>
+        <div className="h-96 md:h-72 w-full md:w-72 relative overflow-hidden">
+            <motion.div
+                initial={false}
+                variants={hoverVariant}
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+                animate={isHovering ? 'open' : 'closed'}
+            >
+                <Button
+                    isImage
+                    image="/media/piano-photos/grand-piano.jpeg"
+                    alt="Grand Piano"
+                    isLink
+                    link="/"
+                    className="h-96 md:h-72 w-full md:w-72 object-cover"
+                />
+            </motion.div>
+            <div
+                className="h-96 md:h-48 w-full md:w-72 bg-gradient-to-t from-black absolute bottom-0"
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+            >
+                <Button
+                    isLink
+                    link="/"
+                    text="Grand Pianos"
+                    className="text-white font-medium text-center text-4xl md:text-2xl tracking-wide mt-60 md:mt-28"
+                />
             </div>
-            <Button
-                isImage
-                image="/media/piano-photos/grand-piano.jpeg"
-                alt="Grand Piano"
-                isLink
-                link="/"
-                className="h-72 w-72 object-cover rounded-full"
-            />
         </div>
     );
 };
