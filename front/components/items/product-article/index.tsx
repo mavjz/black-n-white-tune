4ffea067c8e673a510f35ec1b167e7f73a7df2a8
mavjz28 <maja.miaskiewicz@gmail.com>
2024-02-22 16:22:41 +0100

    fix: ProductArticle tablet view

--- front/components/items/product-article/index.tsx
+++ front/components/items/product-article/index.tsx
@@ -12,20 +12,20 @@ const ProductArticle = ({
 }: ProductArticleType) => {
     return (
         <div
-            className={`flex ${imageRight ? 'md:flex-row-reverse' : 'md:flex-row'} flex-col mt-12`}
+            className={`flex ${imageRight ? 'lg:flex-row-reverse' : 'lg:flex-row'} flex-col mt-12`}
         >
-            <ImageFramed className="w-full md:w-1/2 h-96 object-cover" src={image} alt={title} />
-            <div className="w-full md:w-1/2 h-96 px-5 md:px-32 flex flex-col items-center md:items-start">
+            <ImageFramed className="w-full lg:w-1/2 h-96 object-cover" src={image} alt={title} />
+            <div className="w-full lg:w-1/2 h-fit lg:h-96 pb-6 lg:pb-0 px-5 lg:px-24 flex flex-col items-center lg:items-start">
                 <h3 className="text-2xl font-bold font-nunito mt-2 text-red-950 tracking-wide">
                     {pretitle}
                 </h3>
-                <h2 className="text-3xl font-bold my-4 text-center md:text-left">{title}</h2>
-                <p className="text-sm mb-6 text-center md:text-left">{description}</p>
+                <h2 className="text-3xl font-bold my-4 text-center lg:text-left">{title}</h2>
+                <p className="text-sm mb-6 text-center lg:text-left">{description}</p>
                 <Button
                     isLink
                     link="/"
                     text="Read more"
-                    className="h-10 w-52 rounded-full bg-red-950 md:text-xl text-white flex items-center justify-center"
+                    className="h-10 w-52 rounded-full bg-red-950 lg:text-xl text-white flex items-center justify-center"
                 />
             </div>
         </div>
