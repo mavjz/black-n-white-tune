97f6ca1431b7bea6894052d15a631fce3bb46c47
mavjz28 <maja.miaskiewicz@gmail.com>
2024-02-26 12:27:17 +0100

    feat: add productcategory

--- front/components/items/product-category/index.tsx
+++ front/components/items/product-category/index.tsx
@@ -6,20 +6,31 @@ import { ProductCategoryType } from './models';
 const ProductCategory = ({ model, brand, price, image, link }: ProductCategoryType) => {
     return (
         <div>
-            <motion.div>
-                <Button
-                    isImage
-                    image={image}
-                    alt={model + ' ' + brand}
-                    isLink
-                    link={link}
-                    className=""
-                />
-            </motion.div>
-            <div>
-                <h3>{model}</h3>
-                <p>{brand}</p>
-                <h5>{price}</h5>
+            <div className="w-full md:w-1/2 lg:w-1/3 h-4/5 relative overflow-hidden">
+                <motion.div
+                    whileHover={{
+                        scale: 1.2,
+                        transition: {
+                            duration: 1,
+                        },
+                    }}
+                >
+                    <Button
+                        isImage
+                        image={image}
+                        alt={model + ' ' + brand}
+                        isLink
+                        link={link}
+                        className="w-full h-[29rem] object-fill"
+                    />
+                </motion.div>
+            </div>
+            <div className="px-5 my-4 md:my-2">
+                <h3 className="text-4xl font-gruppo font-black hover:underline decoration-2">
+                    {model}
+                </h3>
+                <p className="">{brand}</p>
+                <h5 className="mt-3">{price}</h5>
             </div>
         </div>
     );
--- front/pages/category.tsx
+++ front/pages/category.tsx
@@ -1,3 +1,4 @@
+import ProductCategory from '@/components/items/product-category';
 import React from 'react';
 
 const Category = ({ category }) => {
@@ -7,6 +8,13 @@ const Category = ({ category }) => {
             <h1 className="text-4xl mx-5 my-4 font-gruppo font-black border-b-2 border-red-950/[0.8] pb-1">
                 {category}
             </h1>
+            <ProductCategory
+                model="A-188"
+                brand="Steinway and sons"
+                price="5600 zł"
+                link="/"
+                image="/media/piano-photos/grand-piano.jpeg"
+            />
         </div>
     );
 };
