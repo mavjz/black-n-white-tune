e19519356a2649ed79214378828e7745a754e6de
mavjz28 <maja.miaskiewicz@gmail.com>
2024-03-10 21:20:42 +0100

    feat: ButtonCategory mobile view add + animation + in progress

--- front/components/items/button-category/index.tsx
+++ front/components/items/button-category/index.tsx
@@ -4,11 +4,13 @@ import TuneIcon from '@mui/icons-material/Tune';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { Field, Form, Formik } from 'formik';
+import { motion } from 'framer-motion';
 
 const ButtonCategory = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [isPriceShown, setIsPriceShown] = useState(false);
     const [isBrandShown, setIsBrandShown] = useState(false);
+
     const priceArray = [
         { value: 'to1000', label: '0zł - 1.000zł' },
         { value: 'to5000', label: '1.000zł - 5.000zł' },
@@ -28,6 +30,25 @@ const ButtonCategory = () => {
         'Yamaha',
     ];
 
+    const categoryMenuVariants = {
+        open: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                bounce: 0,
+                duration: 1,
+            },
+        },
+        closed: {
+            opacity: 0,
+            x: '-100%',
+            transition: {
+                bounce: 0,
+                duration: 0.5,
+            },
+        },
+    };
+
     useEffect(() => {
         if (isOpen) {
             document.body.classList.add('overflow-y-hidden');
@@ -35,10 +56,16 @@ const ButtonCategory = () => {
             document.body.classList.remove('overflow-y-hidden');
         }
     });
+
     return (
         <React.Fragment>
-            <div className="absolute bg-slate-950/[0.5] w-full h-screen top-0 z-20">
-                <div className="bg-slate-50 w-2/5 h-screen absolute right-0 px-8 pt-5">
+            <motion.div
+                animate={isOpen ? 'open' : 'closed'}
+                initial={false}
+                variants={categoryMenuVariants}
+                className="absolute bg-slate-950/[0.5] w-full h-screen top-0"
+            >
+                <div className="bg-slate-50 w-full lg:w-2/5 h-screen absolute right-0 px-8 pt-5">
                     <div className="w-full flex flex-row justify-between pb-2 border-b-red-950 border-b-2">
                         <h1 className="text-4xl font-gruppo font-semibold">Filter & Sort</h1>
                         <Button
@@ -55,7 +82,11 @@ const ButtonCategory = () => {
                     >
                         {({ values }) => (
                             <Form className="flex flex-col">
-                                <Field as="select" name="sort">
+                                <Field
+                                    as="select"
+                                    name="sort"
+                                    className="w-full h-12 my-5 px-4 py-2 border border-slate-900 focus-visible:outline-none bg-slate-50"
+                                >
                                     <option value="default">Default</option>
                                     <option value="cheap">Price low to high</option>
                                     <option value="expensive">Price high to low</option>
@@ -86,11 +117,8 @@ const ButtonCategory = () => {
                                             key={index}
                                             htmlFor="price"
                                             className={`border-2 border-red-950 rounded-full px-5 py-2 relative
-                                        ${
-                                            values.price.includes(checkbox.value) &&
-                                            'bg-red-950 text-white'
-                                        }
-                                        `}
+                                ${values.price.includes(checkbox.value) && 'bg-red-950 text-white'}
+                                `}
                                         >
                                             <Field
                                                 type="checkbox"
@@ -128,8 +156,8 @@ const ButtonCategory = () => {
                                             key={index}
                                             htmlFor="brand"
                                             className={`border-2 border-red-950 rounded-full px-5 py-2 relative
-                                        ${values.brand.includes(brand) && 'bg-red-950 text-white'}
-                                        `}
+                                ${values.brand.includes(brand) && 'bg-red-950 text-white'}
+                                `}
                                         >
                                             <Field
                                                 type="checkbox"
@@ -145,7 +173,8 @@ const ButtonCategory = () => {
                         )}
                     </Formik>
                 </div>
-            </div>
+            </motion.div>
+
             <Button
                 isMUIIcon
                 onClick={() => setIsOpen(!isOpen)}
--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -24,5 +24,6 @@ const config: Config = {
         },
     },
     plugins: [],
+    important: true,
 };
 export default config;
